defa3309fb7e6d31f12f734d07d88a2252e54016
Peter Herczku <peterherczku915@gmail.com>
2025-05-04 19:17:07 +0200

    ft: updated to SDK 53

--- app/index.tsx
+++ app/index.tsx
@@ -6,11 +6,10 @@ import { ActivityIndicator, View } from "react-native";
 
 export default function IndexPage() {
 	const { user, isLoaded } = useUser();
-	const rootNavigationState = useRootNavigationState();
 	const router = useRouter();
 
 	useEffect(() => {
-		if (!rootNavigationState?.key || !isLoaded) return;
+		if (!isLoaded) return;
 
 		const role = user?.publicMetadata?.role;
 
@@ -19,7 +18,7 @@ export default function IndexPage() {
 		} else {
 			router.replace("/(worker)/(tabs)/");
 		}
-	}, [rootNavigationState?.key, isLoaded]);
+	}, [isLoaded]);
 
 	return (
 		<View className="flex-1 justify-center items-center">
